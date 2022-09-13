2953a61abcb10c866870c82f10c45ff64367c429
Shahid77137 <siddiquibabuddin202@gmail.com>
2022-09-14 00:10:44 +0530

    add pallindrome and prime

---
+++ pallindrom.js
@@ -0,0 +1,13 @@
+let num=12321;
+ let rev=0;
+ let tem=num;
+ while(num>0){
+    rem=num%10;
+    num=Math.floor(num/10);
+    rev=rev*10+rem;
+    }
+    if(num==rev){
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }
---
+++ prime.js
@@ -0,0 +1,15 @@
+
+	let num=23;
+
+	let fact=0;
+
+	for(let i=1;i<=num;i++){
+		if(num%i==0){
+		fact=1;
+		}
+	}
+	if(fact==2){
+	console.log(num,"is prime num");
+	}else{
+	console.log(num,"is not prim num);
+	}
